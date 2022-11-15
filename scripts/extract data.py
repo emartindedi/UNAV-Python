db7c53b9d0fcc7a4d6fe2a7f650dd4d62306b547
Elena Martin de Diego <emartindedi@alumni.unav.es>
2022-11-15 22:25:33 +0100

    Update

--- scripts/extract data.py
+++ scripts/extract data.py
@@ -4,7 +4,7 @@ import pandas as pd
 import krakenex
 from pykrakenapi import KrakenAPI # Note: pykrakenapi requires Python >= 3.3, krakenex >= 2.0.0 and pandas.
 
-def get_currencies(dir_path, name_file):
+def save_currencies(dir_path, name_file):
     """save into a csv file the wsname and altname of the currencies availables in the kraken API
     : dir_path: path of the folder (directory) where it will be saved
     : name_file: name (without .csv) of the file where it will be saved"""
@@ -32,7 +32,7 @@ def ask_user_for_currency(df):
             if user_curr in df.index:
                 break
             else:
-                print("The currency is not available or it not correct.")
+                print("The currency is not available or it not correct")
         except ValueError:
             print('Invalid')
             continue
@@ -44,21 +44,12 @@ def user_select_currency(df, curr):
     : curr: a string which contains the wsname of the currency selected by the user
     : return: a tuple object which contains the dataframe for the currency curr and the last time"""
     return (k.get_ohlc_data(df.loc[curr]['altname'], ascending=True))
-#
 
 
 # -------------------------------------------------------------------------
-
-if __name__ == "__main__":
-    api = krakenex.API()  # Instance of the krakenex.API class
-    k = KrakenAPI(api)  # Implements the Kraken API methods using the low-level krakenex python
-
-    # Decide the names of the directory for the outputs and the name of the file containing the names of the currencies
-    dir_path = 'results'
-    name_file = 'Possible_Currencies'
-
+def get_dataframe_to_process(dir_path, name_file, data_name):
     # Obtain a csv file with all names of available currencies
-    get_currencies(dir_path, name_file)
+    save_currencies(dir_path, name_file)
 
     # Save the currencies in a pandas dataframe
     currencies_names_df = pd.read_csv('{}.csv'.format(os.path.join(dir_path, name_file)), index_col=[0])
@@ -66,10 +57,27 @@ if __name__ == "__main__":
     # Ask the user for the currency wanted to analyze
     user_curr = ask_user_for_currency(currencies_names_df)
 
-    # Print
-    (cur1, cur2) = user_select_currency(currencies_names_df, user_curr)
+    try:
+        if not os.path.isdir(dir_path):
+            os.makedirs(dir_path) # If it does not exist the directory, we create it
+        data = user_select_currency(currencies_names_df, user_curr)[0]
+        data.to_csv('{}.csv'.format(os.path.join(dir_path, data_name))) # Save it
+        print("Saved correctly")
+    except OSError:
+        print("The name of the file or the directory are incorrect")
 
-    print(cur1.head())
-    print(cur1.tail())
+    return user_select_currency(currencies_names_df, user_curr)
 
 
+
+if __name__ == "__main__":
+    api = krakenex.API()  # Instance of the krakenex.API class
+    k = KrakenAPI(api)  # Implements the Kraken API methods using the low-level krakenex python
+
+    # Decide the names of the directory for the outputs and the name of the file containing the names of the currencies
+    dir_path = 'data'
+    name_file = 'Possible_Currencies'
+    data_name = 'dataset_to_process'
+
+    # Save the dataframe wanted by the user in a file called 'dataset_to_process.csv' in data directory
+    get_dataframe_to_process(dir_path, name_file, data_name)
